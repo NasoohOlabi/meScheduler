f181bffb11c1c03d643b982824744c443030524d
NasoohOlabi <nasooholabi@gmail.com>
2021-10-08 15:51:38 +0200

    Added Data ParserView direction support and UiTexts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,83 @@
-import React from 'react';
-import './App.css';
-import {TimeRemaining} from "./ETAScreen";
+import React from "react";
+import "./App.css";
+import { TimeRemaining } from "./ETAScreen";
 import MyAppBar from "./Components/AppBar";
-import {lightTheme , darkTheme} from "./theme/themes";
+import { lightTheme, darkTheme } from "./theme/themes";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import ReactPWAInstallProvider from "react-pwa-install";
-import {WeekView} from './Components/Week';
+import { WeekView } from "./Components/Week";
+import { DataParserView } from "./DataParserView";
+import { Screen } from "./Interfaces/Interfaces";
+import { texts } from "./Components/UiText";
 
+const HomeScreen: Screen = Screen.DATAPARSER;
 
-enum Screen  {ETA , TABLE}
-
-const HomeScreen : Screen = Screen.TABLE;
-
-
-
-
-function Body (props : {UI:Screen} ) : JSX.Element{
-  let comp = (props.UI === Screen.ETA) ?<TimeRemaining/>:<WeekView />;
-  return (
-        comp
-  );
+function Body(props: { UI: Screen }): JSX.Element {
+	switch (props.UI) {
+		case Screen.ETA:
+			return <TimeRemaining />;
+		case Screen.TABLE:
+			return <WeekView />;
+		case Screen.DATAPARSER:
+			return <DataParserView />;
+	}
 }
 
+function App(): JSX.Element {
+	const [screen, setScreen] = React.useState(HomeScreen);
+	const [dark, setDark] = React.useState(true);
+	const [lang, setLang] = React.useState(texts.LangDirection);
+	const switchViewTo =
+		(s: "ETA" | "TABLE" | "DATAPARASER") => (event: any) => {
+			switch (s) {
+				case "ETA":
+					setScreen(Screen.ETA);
+					break;
+				case "TABLE":
+					setScreen(Screen.TABLE);
+					break;
+				case "DATAPARASER":
+					setScreen(Screen.DATAPARSER);
+					break;
+			}
+		};
 
-function App() :JSX.Element {
-  const [screen , setScreen] = React.useState(HomeScreen);
-  const [dark , setDark] = React.useState(true);
+	const toggleTheme = (event: any) => {
+		setDark(!dark);
+	};
+	const toggleLang = (event: any) => {
+		if (lang === "ltr") {
+			setLang("rtl");
+		} else {
+			setLang("ltr");
+		}
+	};
 
-  const switchToETA = (event : any) =>{
-    if (screen !== Screen.ETA){
-      setScreen(Screen.ETA);
-    }
-  }
+	let theme = () => (dark ? { ...darkTheme } : { ...lightTheme });
 
-  const toggleTheme = (event : any)=>{
-    setDark(!dark);
-  }
- 
-  const switchToTable = (event : any) =>{
-    if (screen !== Screen.TABLE){
-      setScreen(Screen.TABLE);
-    }
-  }
-
-  let theme = ()=> (dark)?{...darkTheme} :  {...lightTheme};
-
-  return (
-    <div>
-    <ThemeProvider theme={theme()}   >
-      <CssBaseline />
-      <ReactPWAInstallProvider enableLogging>
-      <MyAppBar theme={theme()}  UI={screen} toggleTheme={toggleTheme} darkThemed={dark} switchToTable={switchToTable} switchToETA = {switchToETA} />
-      </ReactPWAInstallProvider>
-      <Body UI={screen} />
-    </ThemeProvider>
-    </div>
-  );
+	return (
+		<div>
+			<ThemeProvider theme={theme()}>
+				<CssBaseline />
+				<ReactPWAInstallProvider enableLogging>
+					<MyAppBar
+						theme={theme()}
+						UI={screen}
+						toggleTheme={toggleTheme}
+						darkThemed={dark}
+						switchToTable={switchViewTo("TABLE")}
+						switchToETA={switchViewTo("ETA")}
+						switchToDataParser={switchViewTo("DATAPARASER")}
+						toggleLang={toggleLang}
+					/>
+				</ReactPWAInstallProvider>
+				<div dir={lang}>
+					<Body UI={screen} />
+				</div>
+			</ThemeProvider>
+		</div>
+	);
 }
-
 
 export default App;
