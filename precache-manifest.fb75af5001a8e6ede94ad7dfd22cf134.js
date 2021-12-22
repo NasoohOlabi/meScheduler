43293e68ef95bb48407b38a810958c5ed7175403
NasoohOlabi <nasooholabi@gmail.com>
2021-12-22 15:59:14 +0200

    Updates

---
+++ precache-manifest.fb75af5001a8e6ede94ad7dfd22cf134.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd5134601bad6979f859",
+    "url": "/meScheduler/static/css/main.1da3faa9.chunk.css"
+  },
+  {
+    "revision": "cd5134601bad6979f859",
+    "url": "/meScheduler/static/js/main.9df245a8.chunk.js"
+  },
+  {
+    "revision": "47858aebaac8dffcf9ae",
+    "url": "/meScheduler/static/js/runtime~main.bcfb9d84.js"
+  },
+  {
+    "revision": "354dcaefb4c915ba0516",
+    "url": "/meScheduler/static/css/2.0ad3eee3.chunk.css"
+  },
+  {
+    "revision": "354dcaefb4c915ba0516",
+    "url": "/meScheduler/static/js/2.3385a68f.chunk.js"
+  },
+  {
+    "revision": "1251e671ea3df5f025d5",
+    "url": "/meScheduler/static/js/3.8a8bb745.chunk.js"
+  },
+  {
+    "revision": "f646d2f166400e94cf5fce8af467f574",
+    "url": "/meScheduler/static/js/solve.worker.fd53a5b700a3f01679a9.js"
+  },
+  {
+    "revision": "1dd423a49c0f453548be609581a079ba",
+    "url": "/meScheduler/index.html"
+  }
+];
